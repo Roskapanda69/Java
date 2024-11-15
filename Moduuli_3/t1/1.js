c6e8a78db8fc12cd68c9c7b4dc3eafc24259dae4
Roskapanda69 <markku.pyrro@metropolia.com>
2024-11-15 13:06:23 +0200

    lulle

---
+++ Moduuli_3/t1/1.js
@@ -0,0 +1,9 @@
+const add_thing = document.querySelector('#target');
+
+add_thing.innerHTML = `
+<li>First item</li>
+<li>Second item</li>
+<li>Third item</li>
+`;
+
+add_thing.className = 'my-list'
